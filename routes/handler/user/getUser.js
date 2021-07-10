fb168ab196296b2eb8863940af1f7d04963d3633
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2021-07-10 22:28:20 +0700

    integrasi api gateway get profile

---
+++ routes/handler/user/getUser.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const {
+    URL_SERVICE_USER
+} = process.env;
+
+module.exports = async (req, res) => {
+    try {
+        const id = req.user.data.id;
+        const response = await axios.get(`${URL_SERVICE_USER}/users/${id}`);
+        return res.json(response.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({
+                status: "error",
+                message: "service unavailable"
+            });
+        }
+
+        const {
+            status,
+            data
+        } = error.response;
+
+        return res.status(status).json(data);
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const verifyToken = require('../middleware/verify-token');
 router.post('/register', userHandler.register);
 router.post('/login', userHandler.login);
 router.put('/', verifyToken, userHandler.update);
+router.get('/', verifyToken, userHandler.getUser);
 
 module.exports = router;
