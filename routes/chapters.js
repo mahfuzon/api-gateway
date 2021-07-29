77ff59083f616de29090413723f3b0b9346bfdbe
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2021-07-29 12:25:41 +0700

    integrasi dengan api chapter part 2

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const chaptersHandler = require('./handler/chapters');
-const verifyToken = require('../middleware/verify-token');
 
 router.post('/', chaptersHandler.create);
 router.put('/:id', chaptersHandler.update);
 router.delete('/:id', chaptersHandler.destroy);
+router.get('/', chaptersHandler.getAll);
+router.get('/:id', chaptersHandler.get);
 
 module.exports = router;
--- routes/handler/chapters/getAll.js
+++ routes/handler/chapters/getAll.js
@@ -1,48 +1,26 @@
-// require('dotenv').config();
-// const axios = require('axios');
+require('dotenv').config();
+const axios = require('axios');
 
-// module.exports = async (req, res) => {
-//     try {
-//         const url = process.env.URL_SERVICE_COURSE;
-//         const HOSTNAME = process.env.HOSTNAME;
-//         const response = await axios.get(`${url}/api/courses/`, {
-//             params: {
-//                 name: req.query.name,
-//                 status: 'published'
-//             }
-//         });
-
-//         const courseData = response.data;
-//         const firstPage = courseData.data.first_page_url.split('?').pop();
-//         const lastPage = courseData.data.last_page_url.split('?').pop();
-
-//         courseData.data.first_page_url = `${HOSTNAME}/courses?/${firstPage}`;
-//         courseData.data.last_page_url = `${HOSTNAME}/courses?/${lastPage}`;
-
-//         if (courseData.data.next_page_url) {
-//             const nextPage = courseData.data.next_page_url.split('?').pop();
-//             courseData.data.next_page_url = `${HOSTNAME}/courses?${nextPage}`;
-//         }
-
-//         if (courseData.data.prev_page_url) {
-//             const prevPage = courseData.data.prev_page_url.split('?').pop();
-//             courseData.data.prev_page_url = `${HOSTNAME}/courses?${prevPage}`;
-//         }
-
-//         courseData.data.path = `${HOSTNAME}/courses`;
-
-//         return res.json(courseData);
-//     } catch (error) {
-//         if (error.code === 'ECONNREFUSED') {
-//             return res.status(500).json({
-//                 status: 'error',
-//                 message: 'service unavailable'
-//             });
-//         }
-//         const {
-//             status,
-//             data
-//         } = error.response;
-//         return res.status(status).json(data);
-//     }
-// }
+module.exports = async (req, res) => {
+    try {
+        const url = process.env.URL_SERVICE_COURSE;
+        const response = await axios.get(`${url}/api/chapters`, {
+            params: {
+                course_id : req.params.course_id
+            }
+        });
+        return res.json(response.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({
+                status: 'error',
+                message: 'service unavailable'
+            });
+        }
+        const {
+            status,
+            data
+        } = error.response;
+        return res.status(status).json(data);
+    }
+}
